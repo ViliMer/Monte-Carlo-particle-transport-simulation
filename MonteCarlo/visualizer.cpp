16938439c2053e0de0ae70490f3bd002c7d34827
ViliMer <vili.merilainen@hotmail.com>
2020-10-25 19:03:21 +0200

    WIP visulizer

--- MonteCarlo/visualizer.cpp
+++ MonteCarlo/visualizer.cpp
@@ -4,7 +4,11 @@
 
 void render(vector<cell> &cells)
 {
-	sf::RenderWindow window(sf::VideoMode(800, 1200), "Render");
+	
+	sf::Vector3f camera_pos(-5, 0, 0);
+	sf::Vector3f camera_dir(1, 0, 0);
+
+	sf::RenderWindow window(sf::VideoMode(1200, 1200), "Render");
 	sf::Image current_image;
 	current_image.create(800, 1200, sf::Color::White);
 	sf::Texture texture;
@@ -26,7 +30,7 @@ void render(vector<cell> &cells)
 
 		window.clear(sf::Color::White);
 		for (int x = 0; x < 1200; x++) {
-			for (int y = 0; y < 800; y++) {
+			for (int y = 0; y < 1200; y++) {
 				if (sqrt((x-200)*(x-200) + (y-300)*(y-300)) < 200) {
 					current_image.setPixel(x, y, sf::Color::Blue);
 				}
@@ -36,4 +40,21 @@ void render(vector<cell> &cells)
 		window.draw(sprite);
 		window.display();
 	}
+}
+
+
+camera::camera() {}
+
+camera::camera(sf::Vector3f pos, sf::Vector3f dir, int xresolution, int yresolution)
+{
+	camera_pos = pos;
+	camera_dir = dir;
+
+	xres = xresolution;
+	yres = yresolution;
+}
+
+sf::Vector3f camera::pos_screen(int x, int y) {
+	// WIP
+	return (sf::Vector3f(xres - xres / 2 - x, yres - yres / 2 - y, 0));
 }
--- MonteCarlo/visualizer.h
+++ MonteCarlo/visualizer.h
@@ -5,3 +5,18 @@
 using namespace std;
 
 void render(vector<cell> &cells);
+
+class camera
+{
+public:
+	sf::Vector3f camera_pos = sf::Vector3f(-5,0,0);
+	sf::Vector3f camera_dir = sf::Vector3f(1,0,0);
+
+	int xres = 1200;
+	int yres = 800;
+
+	camera();
+	camera(sf::Vector3f pos, sf::Vector3f dir, int xresolution, int yresolution);
+
+	sf::Vector3f pos_screen(int x, int y);
+};
